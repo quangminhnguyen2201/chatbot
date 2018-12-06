2c67b106124293d12ae0d33f87f5ad5ca8a7a13e
Quang Minh <quangminhnguyen2201@gmail.com>
2018-12-06 21:55:59 +0700

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const fs = require("fs");
 const login = require("facebook-chat-api");
 const readline = require("readline");
- 
-var answeredThreads = {};
+
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+var answeredThreads = {};
  
 // Create simple echo bot
 login({email: "minhnq22", password: "gt3q2-fls09-D12M5-##98#"}, function callback (err, api) {
-    // if(err) return console.error(err);
     if(err) {
         switch (err.error) {
             case 'login-approval':
@@ -27,10 +26,10 @@ login({email: "minhnq22", password: "gt3q2-fls09-D12M5-##98#"}, function callbac
     }
 
     api.listen(function callback(err, message) {
-        console.log(message.threadID);
-        if(!answeredThreads.hasOwnProperty(message.threadID)){
-            answeredThreads[message.threadID] = true;
-            api.sendMessage("Chào! Xin lỗi vì hiện tại mình không thể trả lời vào lúc này. Nếu cần thiết, bạn có thể liên hệ trực tiếp vào số điện thoại của mình hoặc để lại mail vào địa chỉ quangminhnguyen2201@gmail.com. Cảm ơn!");
-        }
+        // console.log(message.threadID);
+        // if(!answeredThreads.hasOwnProperty(message.threadID)){
+        //     answeredThreads[message.threadID] = true;
+            api.sendMessage("Chào! Xin lỗi vì mình không thể trả lời vào thời điểm này. Nếu cần thiết, bạn có thể liên hệ trực tiếp vào số điện thoại của mình hoặc để lại lời nhắn vào địa chỉ quangminhnguyen2201@gmail.com. Cảm ơn!", message.threadID);
+        // }
     });
 });
