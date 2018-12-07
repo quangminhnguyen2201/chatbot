97a75df5909fea72b3aab680cedf052419807d66
Nguyễn Quang Minh <quangminhnguyen2201@gmail.com>
2018-12-07 17:18:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+const fs = require("fs");
 const login = require("facebook-chat-api");
 const readline = require("readline");
-
+var myID = 100006280864661;
+var answeredThreads = {};
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-var answeredThreads = {};
+var nameSender;
  
 // Create simple echo bot
-login({email: "", password: ""}, function callback (err, api) {
+login({email: "minhnq22", password: "gt3q2-fls09-D12M5-##98#"}, function callback (err, api) {
     if(err) {
         switch (err.error) {
             case 'login-approval':
@@ -24,12 +25,25 @@ login({email: "", password: ""}, function callback (err, api) {
         }
         return;
     }
+    api.setOptions({forceLogin: true, selfListen: true, logLevel: "silent"});
 
     api.listen(function callback(err, message) {
-        // console.log(message.threadID);
-        // if(!answeredThreads.hasOwnProperty(message.threadID)){
-        //     answeredThreads[message.threadID] = true;
-            api.sendMessage("Chào! Xin lỗi vì mình không thể trả lời vào thời điểm này. Nếu cần thiết, bạn có thể liên hệ trực tiếp vào số điện thoại của mình hoặc để lại lời nhắn vào địa chỉ quangminhnguyen2201@gmail.com. Cảm ơn!", message.threadID);
-        // }
+        if(err) return console.error(err);
+        api.getUserInfo(message.senderID, function(err, ret) {
+            if (err) return console.error(err);
+            for (var prop in ret) {
+                if (ret.hasOwnProperty(prop) && ret[prop].name) {
+                    nameSender = ret[prop].name;
+                }
+            }
+        })
+        console.log("***From", nameSender, ":");
+        console.log(message.body);
+        console.log("----------");
+        if (!message.isGroup && message.senderID != myID) {
+            console.log(">>>Send to", nameSender, ":");
+            console.log("----------");
+            api.sendMessage("Chào! Xin lỗi vì mình không thể trả lời tin nhắn vào thời điểm này. Nếu cần thiết, bạn có thể liên hệ trực tiếp vào số điện thoại của mình hoặc để lại lời nhắn vào địa chỉ quangminhnguyen2201@gmail.com. Cảm ơn!", message.threadID);
+        }
     });
 });
